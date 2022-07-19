2a85fbb73b70d0cbe4b4669d1188cf521d553bd1
shipolapka <mamulatik@gmail.com>
2022-07-19 22:05:31 +0400

    PDO

--- controllers/MainController.php
+++ controllers/MainController.php
@@ -3,11 +3,21 @@
 namespace controllers;
 
 use core\Controller;
+use lib\Db;
 
 class MainController extends Controller
 {
     public function indexAction()
     {
+
+        $db = new Db;
+
+        $data = $db->column('SELECT name FROM user WHERE id=1');
+        debug($data);
+//        $vars = [
+//            'name' => 'Остя',
+//            'age' => 30,
+//        ];
         $this->view->render('Главная страница');
     }
 
