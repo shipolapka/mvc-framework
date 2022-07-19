df4cabf838e14b1698c777a810785142ffe1cf8e
shipolapka <mamulatik@gmail.com>
2022-07-19 22:06:31 +0400

    PDO

--- lib/Db.php
+++ lib/Db.php
@@ -2,12 +2,33 @@
 
 namespace lib;
 
+use PDO;
+
 class Db
 {
-    function __construct()
+    protected $db;
+
+    public function __construct()
     {
-        echo "я класс БД";
+       $config= require 'config/db.php';
+       $this->db = new \PDO('mysql:host=' . $config['host'] . ';dbname = ' . $config['dbname'], $config['user'], $config['password']);
     }
 
+    public function query($sql)
+    {
+        $query = $this->db->query($sql);
+        return $query;
+    }
 
+    public function row($sql)
+    {
+        $result = $this->query($sql);
+        return $result->fetchAll;
+    }
+
+    public function column($sql)
+    {
+        $result = $this->query($sql);
+        return $result->fetchColumn();
+    }
 }
